35f223ecbf251a3c5ee0a22ce079d46d434b2d2c
Adal1942 <adautosombra1942@gmail.com>
2023-05-06 12:08:06 -0300

    addBetterResponsiveness commit

--- js/projectBase.js
+++ js/projectBase.js
@@ -136,6 +136,31 @@ const project = [
             </div>
         </div>`,
     },
+    {
+        id: 8,
+        codigo: 
+        `<div class="card">
+            <div class="box">
+                <div class="pendulums">
+                    <span style="--i:0"></span>
+                    <span style="--i:1"></span>
+                    <span style="--i:2"></span>
+                    <span style="--i:3"></span>
+                    <span style="--i:4"></span>
+                    <span style="--i:5"></span>
+                    <span style="--i:6"></span>
+                    <span style="--i:7"></span>
+                    <span style="--i:8"></span>
+                    <span style="--i:9"></span>
+                    <span style="--i:10"></span>
+                    <span style="--i:11"></span>
+                    <span style="--i:12"></span>
+                    <span style="--i:13"></span>
+                    <span style="--i:14"></span>
+                </div>
+            </div>
+        </div>`,
+    },
 ];
 
 const listContainer = document.querySelector("#container");
